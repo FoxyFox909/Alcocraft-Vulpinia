0f755192779511a1bb875978a1bcd6337c5f1371
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-09-12 18:15:06 -0400

    Added Magnet Pilsner Fluid

--- src/main/java/me/auroraluciri/alcocraftvulpinia/AlcocraftVulpinia.java
+++ src/main/java/me/auroraluciri/alcocraftvulpinia/AlcocraftVulpinia.java
@@ -81,6 +81,8 @@ public class AlcocraftVulpinia {
         ItemBlockRenderTypes.setRenderLayer(AlcoFluids.KVASS.get().getSource(), RenderType.translucent());
         ItemBlockRenderTypes.setRenderLayer(AlcoFluids.LEPRECHAUN_CIDER.get(), RenderType.translucent());
         ItemBlockRenderTypes.setRenderLayer(AlcoFluids.LEPRECHAUN_CIDER.get().getSource(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(AlcoFluids.MAGNET_PILSNER.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(AlcoFluids.MAGNET_PILSNER.get().getSource(), RenderType.translucent());
 
         MenuScreens.register(AlcoMenuTypes.KEG_MENU.get(), KegScreen::new);
     }
